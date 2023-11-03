68b17ca4c4938e6b05308194ad469d58c6c5a173
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-11-03 21:55:47 +0530

    test: add refresh, access token tests

--- tests/users/login.spec.ts
+++ tests/users/login.spec.ts
@@ -3,7 +3,9 @@ import { DataSource } from "typeorm";
 
 import app from "../../src/app";
 import { AppDataSource } from "../../src/config/data-source";
+import { RefreshToken } from "../../src/entity/RefreshToken";
 import { User } from "../../src/entity/User";
+import { isJWT } from "../utils";
 
 describe("POST /auth/login", () => {
   let connection: DataSource;
@@ -39,7 +41,7 @@ describe("POST /auth/login", () => {
     });
 
     it("should find the user in the database and return 200 status code", async () => {
-      const userData = {
+      const payload = {
         firstName: "Shivam",
         lastName: "Vijaywargi",
         email: "vjshivam5@gmail.com",
@@ -47,25 +49,29 @@ describe("POST /auth/login", () => {
       };
 
       // Act
-      await request(app).post("/auth/register").send(userData);
-      const response = await request(app).post("/auth/login").send(userData);
+      await request(app).post("/auth/register").send(payload);
+      const response = await request(app).post("/auth/login").send(payload);
 
       const userRepository = connection.getRepository(User);
 
       const user = await userRepository.findOne({
-        where: { email: userData.email },
+        where: { email: payload.email },
       });
 
-      expect(user?.email).toBe(userData.email);
+      expect(user?.email).toBe(payload.email);
       expect(response.statusCode).toBe(200);
     });
 
     it("should match user entered plain password with hashed password", async () => {
       const payload = {
-        email: "one@one.one",
-        password: "12345678",
+        firstName: "Shivam",
+        lastName: "Vijaywargi",
+        email: "vjshivam5@gmail.com",
+        password: "password",
       };
 
+      // Act
+      await request(app).post("/auth/register").send(payload);
       await request(app).post("/auth/login").send(payload);
 
       const userRepository = connection.getRepository(User);
@@ -88,6 +94,73 @@ describe("POST /auth/login", () => {
       const response = await request(app).post("/auth/login").send(payload);
 
       expect(response.statusCode).toEqual(400);
+    });
+
+    it("should return access token and refresh token inside a cookie", async () => {
+      // ARRANGE
+      const payload = {
+        firstName: "Shivam",
+        lastName: "Vijaywargi",
+        email: "one@one.one",
+        password: "password",
+      };
+
+      // ACT
+      let accessToken = null;
+      let refreshToken = null;
+
+      await request(app).post("/auth/register").send(payload);
+      const response = await request(app).post("/auth/login").send(payload);
+
+      interface IHeaders {
+        ["set-cookie"]: string[];
+      }
+
+      // ASSERT
+      const cookies = (response.headers as IHeaders)["set-cookie"] || [];
+
+      cookies.forEach((cookie) => {
+        if (cookie.startsWith("accessToken=")) {
+          accessToken = cookie.split(";")[0].split("=")[1];
+        }
+
+        if (cookie.startsWith("refreshToken=")) {
+          refreshToken = cookie.split(";")[0].split("=")[1];
+        }
+      });
+
+      expect(accessToken).not.toBeNull();
+      expect(refreshToken).not.toBeNull();
+
+      expect(isJWT(accessToken)).toBeTruthy();
+      expect(isJWT(refreshToken)).toBeTruthy();
+    });
+
+    it("should store the refresh token in the database", async () => {
+      // ARRANGE
+      const payload = {
+        firstName: "Shivam",
+        lastName: "Vijaywargi",
+        email: "one@one.one",
+        password: "password",
+      };
+
+      // ACT
+      await request(app).post("/auth/register").send(payload);
+      const response = await request(app).post("/auth/login").send(payload);
+
+      // ASSERT
+      const refreshTokenRepo = connection.getRepository(RefreshToken);
+      // const tokens = await refreshTokenRepo.find();
+
+      const tokens = await refreshTokenRepo
+        .createQueryBuilder("refreshToken")
+        .where("refreshToken.userId = :userId", {
+          userId: (response.body as Record<string, string>).id,
+        })
+        .getMany();
+
+      expect(tokens).toHaveLength(2);
     });
   });
 
