1ff381dc61544f5a7447bc252201177680f49e65
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-11-10 12:28:39 +0530

    feat: add auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { Request } from "express";
+import { expressjwt, GetVerificationKey } from "express-jwt";
+import jwksClient from "jwks-rsa";
+
+import { CONFIG } from "../config";
+
+export default expressjwt({
+  secret: jwksClient.expressJwtSecret({
+    jwksUri: CONFIG.JWKS_URI!,
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+  }) as GetVerificationKey,
+  algorithms: ["RS256"],
+  getToken: (req: Request) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader && authHeader.split(" ")[1] !== "undefined") {
+      const token = authHeader.split(" ")[1];
+
+      if (token) {
+        return token;
+      }
+    }
+
+    type AuthCookie = {
+      accessToken: string;
+    };
+
+    const { accessToken } = req.cookies as AuthCookie;
+
+    return accessToken;
+  },
+});
