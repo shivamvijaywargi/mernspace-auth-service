b9cb862bd0ad37d4fe28e75db26cbf21d43910c5
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-11-30 09:31:33 +0530

    feat: add tenant schema

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -3,8 +3,6 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 import { CONFIG } from ".";
-import { RefreshToken } from "../entity/RefreshToken";
-import { User } from "../entity/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -16,7 +14,7 @@ export const AppDataSource = new DataSource({
   // synchronize should be false in produciton, keep it always false, since we have beforeEach() blcok setup in tests (register.spec.ts)
   synchronize: false,
   logging: false,
-  entities: [User, RefreshToken],
+  entities: ["src/entity/*.ts"],
   migrations: ["src/migrations/*.ts"],
   subscribers: [],
 });
